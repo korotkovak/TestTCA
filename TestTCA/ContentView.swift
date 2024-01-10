1bf91f6adbc836ab1ac08b1034a9c162baff09ec
Kristina Korotkova <k.korotkova@shinobi.software>
2024-01-10 12:31:05 +0400

    Section 1. Create a reducer

--- TestTCA/ContentView.swift
+++ TestTCA/ContentView.swift
@@ -6,6 +6,32 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct CounterFeature {
+    struct State {
+        var count = 0
+    }
+
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+            }
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
