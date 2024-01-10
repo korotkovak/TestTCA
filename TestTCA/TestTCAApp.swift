c7638f4ceeee92f314db7413e1c0c587dd63df18
Kristina Korotkova <k.korotkova@shinobi.software>
2024-01-10 11:48:02 +0400

    Initial Commit

---
+++ TestTCA/TestTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTCAApp.swift
+//  TestTCA
+//
+//  Created by Kristina Korotkova on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
