d95cb2666cf53a568296a6e3d830d6327df6bee1
Kristina Korotkova <k.korotkova@shinobi.software>
2024-01-10 13:29:50 +0400

    Managing a timer

--- TestTCA/CounterView.swift
+++ TestTCA/CounterView.swift
@@ -14,6 +14,7 @@ struct CounterFeature {
         var count = 0
         var fact: String?
         var isLoading = false
+        var isTimerRunning = false
     }
 
     enum Action {
@@ -21,7 +22,11 @@ struct CounterFeature {
         case incrementButtonTapped
         case factButtonTapped
         case factResponse(String)
+        case timerTick
+        case toggleTimerButtonTapped
     }
+
+    enum CancelID { case timer }
 
     var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -48,6 +53,23 @@ struct CounterFeature {
                 state.fact = fact
                 state.isLoading = false
                 return .none
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
@@ -77,6 +99,14 @@ struct CounterView: View {
                     .background(Color.black.opacity(0.1))
                     .cornerRadius(10)
                 }
+
+                Button(viewStore.isTimerRunning ? "Stop timer" : "Start timer") {
+                    viewStore.send(.toggleTimerButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
 
                 Button("Fact") { viewStore.send(.factButtonTapped) }
                     .font(.largeTitle)
