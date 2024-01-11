614f4beaf8832e9979f9abe2a38df17413c7db95
Kristina Korotkova <k.korotkova@shinobi.software>
2024-01-11 13:03:36 +0400

    Drill-down to contact detail

--- TestTCA/Navigation/ContactsFeature.swift
+++ TestTCA/Navigation/ContactsFeature.swift
@@ -13,12 +13,14 @@ struct ContactsFeature {
     struct State: Equatable {
         var contacts: IdentifiedArrayOf<Contact> = []
         @PresentationState var destination: Destination.State?
+        var path = StackState<ContactDetailFeature.State>()
     }
     
     enum Action {
         case addButtonTapped
         case deleteButtonTapped(id: Contact.ID)
         case destination(PresentationAction<Destination.Action>)
+        case path(StackAction<ContactDetailFeature.State, ContactDetailFeature.Action>)
 
         enum Alert: Equatable {
             case confirmDeletion(id: Contact.ID)
@@ -52,10 +54,16 @@ struct ContactsFeature {
             case let .deleteButtonTapped(id: id):
                 state.destination = .alert(.deleteConfirmation(id: id))
                 return .none
+
+            case .path:
+                return .none
             }
         }
         .ifLet(\.$destination, action: \.destination) {
             Destination()
+        }
+        .forEach(\.path, action: \.path) {
+            ContactDetailFeature()
         }
     }
 }
